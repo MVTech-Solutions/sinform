0d56c1807f28b1ca17b6f29219ae865d80f4e355
brenosgomes <breno_g_@hotmail.com>
2021-03-13 18:48:28 -0300

    production

--- knexfile.js
+++ knexfile.js
@@ -3,6 +3,7 @@ require("dotenv").config()
 module.exports = {
     client: 'mysql2',
     connection: {
+      host: "sinform.cxw8fxplh0eg.us-east-1.rds.amazonaws.com",
       database: 'sinform',
       user:     process.env.dbuser,
       password: process.env.dbpassword
