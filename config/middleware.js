c15d741f1eaa4c8bb5e8ed3667220c6561c2e6dc
Marcus Tavares <marcus.tavares@outlook.com>
2021-04-12 13:53:10 -0300

    restricting cors

--- config/middleware.js
+++ config/middleware.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const cors = require('cors');
 
+const corsOptions = {
+    origin: 'https://www.sinform.com.br',
+    optionSuccessStatus: 200
+}
 
 module.exports = app => {
     app.use(express.json())
-    app.use(cors())
+    app.use(cors(corsOptions))
 }
