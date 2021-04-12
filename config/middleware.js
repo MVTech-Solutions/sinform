b3f56b319f67a48f7110452e1324d4683e9d11a0
Marcus Tavares <marcus.tavares@outlook.com>
2021-04-12 14:03:03 -0300

    restricting cors

--- config/middleware.js
+++ config/middleware.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 
 const corsOptions = {
-    origin: (origin, callback) => origin == 'https://www.sinform.com.br' ? callback(null, true) : callback(new Error('Acesso Negado!')),
+    origin: (origin, callback) => origin == 'https://www.sinform.com.br' ? callback(null, true) : 'Acesso Negado!',
     optionSuccessStatus: 200
 }
 
