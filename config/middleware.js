27a7e3f0aff9dad8548988d767c72fe0b6d57adf
Marcus Tavares <marcus.tavares@outlook.com>
2021-04-17 22:31:40 -0300

    making some changes

--- config/middleware.js
+++ config/middleware.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const cors = require('cors');
 
-const corsOptions = {
+/* const corsOptions = {
     origin: (origin, callback) => origin == 'https://www.sinform.com.br' ? callback(null, true) : callback(new Error('Access Denied by CORS!')),
     optionSuccessStatus: 200
-}
+} */
 
 module.exports = app => {
     app.use(express.json())
-    app.use(cors(corsOptions))
+    app.use(cors(/* corsOptions */))
 }
